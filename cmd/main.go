8674a68683d212f232b5a2dca26a299b85894f3d
installer <installer@gmail.com>
2025-09-19 14:35:30 +0700

    remove rate limiter

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"api/spada/internal/handler"
 	"api/spada/internal/utils"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
@@ -14,16 +12,16 @@ func main() {
 
 	app := fiber.New()
 
-	// Rate limiting khusus untuk API endpoints
-	apiLimiter := limiter.New(limiter.Config{
-		Max:        10,
-		Expiration: 1 * time.Minute,
-		KeyGenerator: func(c *fiber.Ctx) string {
-			return c.IP()
-		},
-	})
+	// // Rate limiting khusus untuk API endpoints
+	// apiLimiter := limiter.New(limiter.Config{
+	// 	Max:        10,
+	// 	Expiration: 1 * time.Minute,
+	// 	KeyGenerator: func(c *fiber.Ctx) string {
+	// 		return c.IP()
+	// 	},
+	// })
 
-	app.Use(apiLimiter)
+	// app.Use(apiLimiter)
 
 	// Register routes
 	handler.RegisterRoutes(app)
